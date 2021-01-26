fdd64cffb212ae86ca6d004595990d91a30c003a
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-01-26 10:12:10 +0000

    Architecture

--- app/controllers/admin/artists_controller.rb
+++ app/controllers/admin/artists_controller.rb
@@ -1,3 +1,10 @@
 class Admin::ArtistsController < ApplicationController
   
   before_action :set_artist, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @artists = Artist.all
+  end
+
+  def new
+    @
