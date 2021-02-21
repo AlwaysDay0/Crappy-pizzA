d660a185b97d8930e401157434acc205d7e398e2
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-02-21 08:32:23 +0000

    pylint

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -20,3 +20,32 @@ class Admin::SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     if @song.save
+      redirect_to admin_song_path(@song), notice: 'song was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to admin_song_path(@song), notice: 'song was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song.destroy
+    redirect_to admin_songs_path, notice: 'song was successfully destroyed.'
+  end
+
+  private
+  def set_songs
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title ,:description ,:rating ,:duration ,:price ,:status ,:progress)
+  end
+end
