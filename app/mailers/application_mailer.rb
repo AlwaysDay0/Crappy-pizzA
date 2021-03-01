f674f6be0ece46e43ece9393b4612b97c280adfb
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-03-01 20:52:09 +0000

    Improve names

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,2 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from
+  default from: 'from@example.com'
+  layout 'mailer'
+end
---
+++ app/models/album.rb
@@ -0,0 +1,10 @@
+
+class Album < ApplicationRecord
+  has_and_belongs_to_many :songs, join_table: 'associations'
+  has_and_belongs_to_many :artists, join_table: 'associations'
+  
+  validates :title, presence: true
+  validates :rating, presence: false
+
+  accepts_nested_attributes_for :songs
+  accepts_nested_attributes_for :artists
