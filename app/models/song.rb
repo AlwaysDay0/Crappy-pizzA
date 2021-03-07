db39138113e4566b2dd9b26616039f0e0c68e4ed
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-03-07 08:21:30 +0000

    improve code

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,3 @@
 class Song < ApplicationRecord
-  has_and_belongs_to_many :albums, join_table: 'associations', depende
+  has_and_belongs_to_many :albums, join_table: 'associations', dependent: :delete_all
+  has_and_belongs_to_many :artists, join_table: 'associa
