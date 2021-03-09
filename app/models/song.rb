9ae5c5704dab64b25f990873f036fd105bb34c9c
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-03-09 05:56:10 +0000

    Added a license file

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,6 @@
 class Song < ApplicationRecord
   has_and_belongs_to_many :albums, join_table: 'associations', dependent: :delete_all
-  has_and_belongs_to_many :artists, join_table: 'associa
+  has_and_belongs_to_many :artists, join_table: 'associations', dependent: :delete_all
+
+  validates :title, presence: true
+  
