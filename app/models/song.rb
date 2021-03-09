b36f9ec819a145bbac2df8c0e6686c6f6543d3e5
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-03-09 10:41:48 +0000

    initial commit...

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,6 @@ class Song < ApplicationRecord
   has_and_belongs_to_many :artists, join_table: 'associations', dependent: :delete_all
 
   validates :title, presence: true
-  
+  validates :duration, presence: false
+  validates :rating, presence: false
+  va
