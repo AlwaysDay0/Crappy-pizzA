77cb1a9b8fa1ac434a2ad6f8440ee4dc878e4577
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-07-15 19:36:38 +0000

    Bump prettier from 2.1.1 to 2.1.2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,4 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Do not eager load code on boot.
-  config.ea
+  config.eager_load = false
+
+  # Show full error reports.
+  conf
