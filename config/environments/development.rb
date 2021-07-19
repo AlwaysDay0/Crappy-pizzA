b84eb7e99f6eb778411b0a2adcc07952aba1082c
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-07-19 18:22:47 +0000

    added new styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,4 +22,6 @@ Rails.application.configure do
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
-  
+    config.action_controller.perform_caching = false
+
+    config.cache
