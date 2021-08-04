cdb32479eef9640a86ec7e362b84b58f9fad41c5
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-08-04 07:17:45 +0000

    Fix debug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -47,4 +47,7 @@ Rails.application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
-  config
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  c
