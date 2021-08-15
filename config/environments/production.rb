21afe6fe2be68d8ded64a91f1106bfae86fb0d11
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-08-15 10:35:07 +0000

    Update bower components

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,4 +10,5 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled
+  # Full error reports are disabled and caching is turned on.
+  config.consider_all_requests_local       =
