0f9effc9857d58d3241ac05be2c39ccb27883d5a
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-08-20 21:34:40 +0000

    posts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,4 +14,5 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  # Ensures that a master key has been made available in ei
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This 
