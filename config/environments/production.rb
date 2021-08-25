3a52118ba81def1f0bb780e66d6ccf2ccae18357
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-08-25 18:28:53 +0000

    fix package names

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,4 +26,7 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset 
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # `config.assets.prec
