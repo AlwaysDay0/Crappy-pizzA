878fa6c3a9b485b4745bc4548f31c950b6bb2efb
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-08-25 09:46:39 +0000

    fix build file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,4 +20,6 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enable
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Co
