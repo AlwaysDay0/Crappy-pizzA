de43bf7a20d319748a70eab486265074d6066227
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-09-09 12:30:28 +0000

    Bump version 0.6.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,4 +43,5 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.actio
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_ca
