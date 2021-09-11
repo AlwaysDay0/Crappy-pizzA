c25964b90a89b4a4cdd49389fc1e549942d6ad05
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-09-11 06:57:10 +0000

    Clean up unused files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,4 +44,4 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_ca
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.
