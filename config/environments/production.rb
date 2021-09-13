3773b90a7e6ff0e8379efb1602cd5af895aab889
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-09-13 21:58:17 +0000

    fixes #74

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,4 +61,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_pr
+  # config.active_job.queue_name_prefix = "apple-music_#{Rails.env}"
+
+  config.action_mailer.perform_
