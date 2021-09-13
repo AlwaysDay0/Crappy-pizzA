0452115ab0473e2aea345f1cd0851a6b9175a3a9
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-09-13 20:36:43 +0000

    0.1.26

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,5 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.ac
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_pr
