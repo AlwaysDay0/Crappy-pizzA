f0d759134b73611745f235bf895114ff288af6c1
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-07 07:58:47 +0000

    Simplify example

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,4 +81,6 @@ Rails.application.configure do
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
-  # config.logger = ActiveSupport
+  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+  if ENV["R
