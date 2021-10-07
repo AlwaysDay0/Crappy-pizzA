60b87af3af46ff54c1d643e2fe859180d726bfdf
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-07 11:36:03 +0000

    Update from stable branch.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,5 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
-    config
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
