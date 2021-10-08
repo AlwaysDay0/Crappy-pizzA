9da0216f8beb6187c397bb2674758d2d73532400
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-08 08:29:52 +0000

    r8169: call netif_napi_del at errpaths and at driver unload

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-# Define an application-wide content security pol
+# Define an application-wide content security policy
+# For further information see the following documentation
+# https://
