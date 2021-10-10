4980c2fde3268b1356238f042d25b26e9b2f2bdb
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-10 13:02:30 +0000

    update some deps

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,4 +4,6 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |poli
+# Rails.application.config.content_security_policy do |policy|
+#   policy.default_src :self, :https
+#   policy.font_sr
