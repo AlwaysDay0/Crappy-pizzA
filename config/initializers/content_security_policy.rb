8ea1eef954736d44d81643967c21f78f09ca845b
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-12 07:40:38 +0000

    Added TOC

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,4 +7,6 @@
 # Rails.application.config.content_security_policy do |policy|
 #   policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
-#   policy.img_src     :self, :https, :
+#   policy.img_src     :self, :https, :data
+#   policy.object_src  :none
+#   policy.script_src  :se
