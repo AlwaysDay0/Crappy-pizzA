441c7ffbbfe02d8401ce13d5cb6e3d6cb27d7fcb
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-15 19:48:34 +0000

    Update version and change log

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -13,4 +13,7 @@
 #   policy.style_src   :self, :https
 
 #   # Specify URI for violation reports
-#   # policy.report_uri
+#   # policy.report_uri "/csp-violation-report-endpoint"
+# end
+
+# If you are using UJS t
