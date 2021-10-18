d7cdb0fd983d00b5cdb5d0cd688e42232aa04bcc
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-18 12:18:10 +0000

    Changed font

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -17,4 +17,4 @@
 # end
 
 # If you are using UJS then enable automatic nonce generation
-# Rails.application.config
+# Rails.application.config.content_security_policy_nonce_generator = -> request { SecureR
