26e98c675b4782c1b517b06a4a8fc54bd6c55150
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-19 10:38:07 +0000

    Checkstyle fixes

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -20,4 +20,5 @@
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
 
 # Report CSP violations to a specified URI
-# For further information see the following documentati
+# For further information see the following documentation:
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cont
