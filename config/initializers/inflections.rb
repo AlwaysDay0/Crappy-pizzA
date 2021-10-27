555e87c1b1595861736f8ede7baf917e085c83f6
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-10-27 17:08:13 +0000

    l10n: Updated Ukrainian (uk) translation to 100%

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -2,4 +2,5 @@
 
 # Add new inflection rules using the following format. Inflections
 # are locale specific, and you may define rules for as many different
-# local
+# locales as you wish. All of these examples are active by default:
+# Activ
