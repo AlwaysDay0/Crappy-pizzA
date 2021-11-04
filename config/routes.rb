abe840438c77784c63851422cc0246a5916ed2b0
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-11-04 22:14:53 +0000

    Bump version: 1.0.0 → 1.0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,28 @@
 
 Rails.application.routes.draw do
+  namespace :api do
+    resources :songs, only: [:index, :show] do
+      get ':resource', to: 'songs#index'
+      patch 'progress', to: 'songs#update'
+      get 'search', on: :collection
+    end
+    resources :artists, only: [:index, :show] do
+      get ':resource', to: 'artists#show'
+      get 'search', on: :collection
+    end
+    resources :albums, only: [:index,:show]  do
+      get ':resource', to: 'albums#show'
+      get 'search', on: :collection
+    end
+  end
+  
+  namespace :admin do
+    root 'home#index'
+    resources :songs
+    resources :artists
+    resources :albums do
+      get "add_record", on: :member
+      patch "save_record", on: :member
+    end
+  end
+end
---
+++ config/spring.rb
@@ -0,0 +1,4 @@
+%w[
+  .ruby-version
+  .rbenv-vars
+  
