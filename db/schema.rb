f12c31312ae471938dd2e6418599d36028f469b2
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2021-12-19 19:45:33 +0000

    Corrected version number.

--- db/schema.rb
+++ db/schema.rb
@@ -3,4 +3,5 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need
+# database schema. If you need to create the application database on another
+# syste
