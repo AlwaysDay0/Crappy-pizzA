dfdd076fa080cec682ee895cdc4f36c11068bc8c
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2022-01-29 17:16:44 +0000

    New home page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 artist1 = Artist.create(name: "Nujabes", age: "31")
 artist2 = Artist.create(name: "Frank Sinatra", age: "43")
 artist3 = Artist.create(name: "Maluma", age: "28")
-artist4 = Artist.c
+artist4 = Artist.create(name: "Taylor Swift", age: "28")
+
+song1 = Song.cr
