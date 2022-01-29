866560b3eda3643ee4c1929cc37965d23eacea41
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2022-01-29 13:12:04 +0000

    fix: merge conflict

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 artist1 = Artist.create(name: "Nujabes", age: "31")
 artist2 = Artist.create(name: "Frank Sinatra", age: "43")
-artist3 =
+artist3 = Artist.create(name: "Maluma", age: "28")
+artist4 = Artist.c
