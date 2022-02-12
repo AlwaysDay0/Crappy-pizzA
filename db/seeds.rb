0c804775fd00d9672729957e149bc40f702113bf
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2022-02-12 13:51:03 +0000

    1.4.15

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@ artist3 = Artist.create(name: "Maluma", age: "28")
 artist4 = Artist.create(name: "Taylor Swift", age: "28")
 
 song1 = Song.create(title: "Imaginary Folklore", duration: "3:50", rating: 5, progress: 0)
-song2 = Song.create(title: "Space dream", dur
+song2 = Song.create(title: "Space dream", duration: "4:50", rating: 5, progress: 0)
+song3 = Song.create(title: "
