b5615cbbec824c711019931b912aaf6349e52151
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-01-08 20:29:54 +0000

    test++

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,4 +5,5 @@
 # files.
 #
 # Given that it is always loaded, you are encouraged to keep this file as
-# light-weight as possible. Requiring heavyweight dependencies from 
+# light-weight as possible. Requiring heavyweight dependencies from this file
+# will add to the boot time of your test suite on EVERY
