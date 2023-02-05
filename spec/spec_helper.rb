7051c72d29b27972f650fb84d97405877c3fb1a7
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-05 12:49:03 +0000

    Some renaming.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,4 +18,5 @@ RSpec.configure do |config|
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
   config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_mes
