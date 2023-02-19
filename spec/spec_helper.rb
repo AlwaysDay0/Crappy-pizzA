d42b24c6be03c1026210e44922817c0696deff8d
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-19 14:07:18 +0000

    Initial Release

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,4 +31,5 @@ RSpec.configure do |config|
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
-    # Prevents you from moc
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a
