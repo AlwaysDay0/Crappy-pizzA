719b16c520e4ad0339ab39bde138ddbe95f4ddb6
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-19 19:17:38 +0000

    gen_stats.c: Duplicate xstats buffer for later use

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -32,4 +32,5 @@ RSpec.configure do |config|
   # library (such as bogus or mocha) by changing the `mock_with` option here.
   config.mock_with :rspec do |mocks|
     # Prevents you from mocking or stubbing a method that does not exist on
-    # a
+    # a real object. This is generally recommended, and will default to
+    # `true` in R
