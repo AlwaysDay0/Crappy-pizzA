d6ba3edb752e4d7685e9c4647e9fa2d582a1fdd3
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-19 13:24:23 +0000

    Update whitespace

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -30,4 +30,5 @@ RSpec.configure do |config|
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
-  config.mock_
+  config.mock_with :rspec do |mocks|
+    # Prevents you from moc
