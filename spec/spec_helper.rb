b0cd3a075e22113def45a9d5b705e1eb17e5ba7a
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-02-27 17:05:38 +0000

    Updated main readme

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,4 +38,4 @@ RSpec.configure do |config|
   end
 
   # This option will default to `:apply_to_host_groups` in RSpec 4 (and will
-  # h
+  # have no way to turn it off -- the option exists only for bac
