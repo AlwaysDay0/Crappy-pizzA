e74e849be7fc7e240ef61c1042b349b42fde7387
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-04-07 18:11:06 +0000

    More readme edits

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,3 +58,5 @@ RSpec.configure do |config|
   # the `--only-failures` and `--next-failure` CLI options. We recommend
   # you configure your source control system to ignore this file.
   config.example_status_persistence_file_path = "spec/examples.txt"
+
+  # Limits the available syntax to the non-monkey patched 
