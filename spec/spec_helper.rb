1fcb8efcaed095a2db5886dd2c121e4288135a59
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-04-23 07:37:21 +0000

    remove unneeded test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -62,4 +62,5 @@ RSpec.configure do |config|
   # Limits the available syntax to the non-monkey patched syntax that is
   # recommended. For more details, see:
   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
-  #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-mes
+  #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
+  #   - http://rspec.info/b
