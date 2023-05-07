9ddda432bff6295b3844017d03711025a7f0ccc9
AlwaysDay0 <106858240+AlwaysDay0@users.noreply.github.com>
2023-05-07 06:10:25 +0000

    Salvaguarda

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -68,4 +68,6 @@ RSpec.configure do |config|
 
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
- 
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the document
